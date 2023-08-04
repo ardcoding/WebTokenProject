d47855f2684fd0d0076924d7c94bcc7f9b66ac1b
Ahmet Rüchan Dora <ahmetdoraa@gmail.com>
2023-08-04 18:02:03 +0300

    Initial commit

---
+++ WebTokenProject/Controllers/TokenController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebTokenProject.Security;
+
+namespace WebTokenProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+            
+        public TokenController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult GetToken()
+        {
+            Token token = TokenHandler.CreateToken(_configuration); 
+            return Ok(token);
+        }
+    }
+}
---
+++ WebTokenProject/Security/TokenHandler.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WebTokenProject.Security
+{
+    public class TokenHandler
+    {
+        public static Token CreateToken(IConfiguration configuration)
+        {
+            Token token = new Token();
+
+            SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(configuration["Token:SecurityKey"]));
+
+            SigningCredentials credentials = new(securityKey, SecurityAlgorithms.HmacSha256);
+
+            token.Expiration = DateTime.Now.AddMinutes(Convert.ToInt16(configuration["Token:Expiration"]));
+
+            JwtSecurityToken jwtToken = new(
+                issuer: configuration["Token:Issuer"],
+                audience : configuration["Token:Audience"],
+                expires: token.Expiration,
+                notBefore: DateTime.Now,
+                signingCredentials: credentials
+                ); 
+            JwtSecurityTokenHandler tokenHandler = new();
+            token.AccessToken = tokenHandler.WriteToken(jwtToken);
+
+            byte[] numbers = new byte[32];
+            using RandomNumberGenerator random = RandomNumberGenerator.Create();
+            random.GetBytes(numbers);
+            token.RefreshToken = Convert.ToBase64String(numbers);
+
+            return token;
+        }
+    }
+}
